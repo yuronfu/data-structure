b23181f4010735b3156e92f57ea65b6b82fab3d4
yuronfu <C14011073@mail.ncku.edu.tw>
2015-10-01 14:13:10 +0800

    finished iterative function

--- nchoosek.c
+++ nchoosek.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int iteration_nchoosek(int n ,int k)
+{
+    int i,result = 1;
+    
+    if(k == 0) return 1;
+    
+    for(i = 1 ; i <= k ; i++) result = result * (n--)/i;
+    
+    return result;
+}
 int main()
 {
     int n,k;
-    int i,m,result = 1;
+    int result;
     
     printf("Compute n choose k, please enter n & k :\n");
     scanf("%d %d",&n,&k);
     
     if(2*k <= n) k = (n-k);
-    m = n;
     
-    for(i = 1; i <= k ; i++)
-    {
-          result = result*i/m;
-    }
-    
+    result = iteration_nchoosek(n,k);
+    printf("result = %d\n",result);
+        
     system("pause");
     return 0;
 }
