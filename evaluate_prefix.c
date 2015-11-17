e987e1ad990202d6b9c2c8fc8ff0704ccb1b613c
yuronfu <C14011073@mail.ncku.edu.tw>
2015-11-17 22:15:58 +0800

    add for require and modify some problem

--- evaluate_prefix.c
+++ evaluate_prefix.c
@@ -4,7 +4,7 @@
 #define  Max_String_length 1024
 #define  Max_Stack_Size    100
 
-typedef enum {rightparenthesis,leftparenthesis,times,divide,plus,minus,mod,operand}precedence;
+typedef enum {rightparenthesis,leftparenthesis,times,divide,plus,minus,mod,operand,space}precedence;
 typedef struct
 {
         int data;
@@ -22,7 +22,7 @@ void push(element item)
 element pop()
 {
     if(top > -1) return stack[top--];
-    else printf("Stack is empty");
+    else printf("Stack is empty\n");
 }
 precedence get_token(char *expr,int *position)
 {
@@ -38,6 +38,7 @@ precedence get_token(char *expr,int *position)
                          case '+': return plus;
                          case '-': return minus;
                          case '%': return mod;
+                         case ' ': return space;
                          default :
                                  printf("invalid symbol in the expression\n");
                                  system("pause");
@@ -45,59 +46,58 @@ precedence get_token(char *expr,int *position)
                          
            }
 }
-int eval(char *expr,int length)
+int eval(char *expr,int length) //透過此函數計算結果並回傳 expr是一個prefix expression，length是最後一個字元的index 
 {
     precedence token;
     element temp;
-    int op1,op2;
+    int op1,op2,digit;
     
-    while(length != -1)
+    while(length != -1) //每讀過一個字元length減1，讀完整個字串length=-1時跳出〈由右往左讀〉
     {
-                 token = get_token(expr,&length);
+                 token = get_token(expr,&length); //透過 get_token讀取目前index字元的類別，參考課本範例 
                  
-                 if(token == operand)
+                 if(token == space) continue; //由於運算元和運算子由空白隔開，讀到空白直接跳到下一次的讀取 
+                 if(token == operand) //讀到運算元時，繼續讀取直到讀到空白 
                  {
-                          temp.data = expr[length+1]-48;
-                          push(temp);
+                          digit = 1;
+                          temp.data = expr[length+1]-48; //由於右往所讀，先讀到的是個位數，接下來讀到的為十位、百位 
+                          while(get_token(expr,&length) != space) temp.data = temp.data + (expr[length+1]-48)*(digit*=10);
+                          push(temp); //讀到空白後，將累計的數值結果push到stack 
                  }
-                 else
+                 else     //不是數字就移除stack最上面的兩個element存到op1,op2中 
                  {
                      op1 = pop().data;
                      op2 = pop().data;
-                     switch(token)
+                     switch(token)    //檢查token是哪一個二元運算子，計算結果並push到stack中 
                      {
                                   case times:
                                        temp.data = op1 * op2;
-                                       push(temp);
                                        break;
                                   case divide:
                                        temp.data = op1 / op2;
-                                       push(temp);
                                        break;
                                   case plus:
                                        temp.data = op1 + op2;
-                                       push(temp);
                                        break;
                                   case minus:
                                        temp.data = op1 - op2;
-                                       push(temp);
                                        break;
                                   case mod:
                                        temp.data = op1 % op2;
-                                       push(temp);
                                        break;
                      }
+                     push(temp);
                  }
     }
-    return pop().data;
+    return pop().data; //由於每次運算後結果都push進stack，所以最終結果也在stack中，直接pop出答案並回傳 
 }
 int main()
 {
     char expr[Max_String_length] = {};
     int result;
     
-    printf("Please enter a prefix expression without no space between each token:\n");
-    scanf("%s",expr);
+    printf("Please enter a prefix expression with space between operator and operand:\n");
+    gets(expr);
     
     result = eval(expr,strlen(expr)-1);
     
--- infix_to_prefix.c
+++ infix_to_prefix.c
@@ -4,7 +4,20 @@
 #define  Max_String_length 1024
 #define  Max_Stack_Size    100
 
-typedef enum {rightparenthesis,leftparenthesis,times,divide,plus,minus,mod,operand}precedence;
+typedef enum {
+        rightparenthesis,leftparenthesis,
+        logicalnot,
+        times,divide,mod,plus,minus,
+        lshift,rshift,
+        less,greater,lessorequal,greaterorequal,
+        equal,notequal,
+        bitwiseand,bitwiseor,
+        logicaland,logicalor,
+        assignment,
+        operand,space
+}precedence;
+int icp[] = {17,17,15,13,13,13,12,12,11,11,10,10,10,10,9,9,8,6,5,4,2};
+int isp[] = {0 ,17,15,13,13,13,12,12,11,11,10,10,10,10,9,9,8,6,5,4,2};
 typedef struct
 {
         precedence data;
@@ -33,11 +46,31 @@ precedence get_token(char *expr,int *position)
            {
                          case ')': return rightparenthesis;
                          case '(': return leftparenthesis;
+                         case '!': return logicalnot;
                          case '*': return times;
                          case '/': return divide;
+                         case '%': return mod;
                          case '+': return plus;
                          case '-': return minus;
-                         case '%': return mod;
+                         case '<':
+                              if(expr[(*position)] == '<') {(*position)--; return lshift;}
+                              else return less;
+                         case '>':
+                              if(expr[(*position)] == '>') {(*position)--; return rshift;}
+                              else return greater;
+                         case '=':
+                              if(expr[(*position)] == '=') {(*position)--; return equal;}
+                              if(expr[(*position)] == '!') {(*position)--; return notequal;}
+                              if(expr[(*position)] == '<') {(*position)--; return lessorequal;}
+                              if(expr[(*position)] == '>') {(*position)--; return greaterorequal;}
+                              else return assignment;
+                         case '&':
+                              if(expr[(*position)] == '&') {(*position)--; return logicaland;}
+                              else return bitwiseand;
+                         case '|':
+                              if(expr[(*position)] == '|') {(*position)--; return logicalor;}
+                              else return bitwiseor;
+                         case ' ': return space;
                          default :
                                  printf("invalid symbol in the expression\n");
                                  system("pause");
@@ -49,11 +82,17 @@ void put_token(char *expr,element temp,int *position)
 {
      switch(temp.data)
      {
+                      case logicalnot:
+                           expr[(*position)++] = '!';
+                           break;
                       case times:
                            expr[(*position)++] = '*';
                            break;
                       case divide:
                            expr[(*position)++] = '/';
+                           break;
+                      case mod:
+                           expr[(*position)++] = '%';
                            break;
                       case plus:
                            expr[(*position)++] = '+';
@@ -61,8 +100,52 @@ void put_token(char *expr,element temp,int *position)
                       case minus:
                            expr[(*position)++] = '-';
                            break;
-                      case mod:
-                           expr[(*position)++] = '%';
+                      case lshift:
+                           expr[(*position)++] = '<';
+                           expr[(*position)++] = '<';
+                           break;
+                      case rshift:
+                           expr[(*position)++] = '>';
+                           expr[(*position)++] = '>';
+                           break;
+                      case less:
+                           expr[(*position)++] = '<';
+                           break;
+                      case greater:
+                           expr[(*position)++] = '>';
+                           break;
+                      case lessorequal:
+                           expr[(*position)++] = '=';
+                           expr[(*position)++] = '<';
+                           break;
+                      case greaterorequal:
+                           expr[(*position)++] = '=';
+                           expr[(*position)++] = '>';
+                           break;
+                      case equal:
+                           expr[(*position)++] = '=';
+                           expr[(*position)++] = '=';
+                           break;
+                      case notequal:
+                           expr[(*position)++] = '=';
+                           expr[(*position)++] = '!';
+                           break;
+                      case bitwiseand:
+                           expr[(*position)++] = '&';
+                           break;
+                      case bitwiseor:
+                           expr[(*position)++] = '|';
+                           break;
+                      case logicaland:
+                           expr[(*position)++] = '&';
+                           expr[(*position)++] = '&';
+                           break;
+                      case logicalor:
+                           expr[(*position)++] = '|';
+                           expr[(*position)++] = '|';
+                           break;
+                      case assignment:
+                           expr[(*position)++] = '=';
                            break;
                       default :
                               printf("invalid symbol in the expression\n");
@@ -71,37 +154,36 @@ void put_token(char *expr,element temp,int *position)
      }
      return;
 }
-void infix_to_prefix(char *expr,char *prefix_expr,int length)
+void infix_to_prefix(char *expr,char *prefix_expr,int length) //prefix_expr儲存轉換後的結果 expr為infix expression 
 {
-     int icp[7] = {17,17,12,12,11,11,12};     //{rightparenthesis,leftparenthesis,times,divide,plus,minus,mod,operand}
-     int isp[7] = {0,17,12,12,11,11,12};
-     int i,count = 0;
+     int i,count = 0; //count:目前prefix_expr的index從0開始 
      char ctemp;
      precedence token;
      element temp;
      
-     while(length != -1)
+     while(length != -1) //每讀過1個字元length減1，讀完整個expr就跳出(由右往左讀) 
      {
-                  token = get_token(expr,&length);
-                  if(token == operand) prefix_expr[count++] = expr[length+1];
-                  else
+                  token = get_token(expr,&length); //透過get_token讀取目前的字元類別 
+                  if(token == space) continue;     //token為空白就跳到下一次讀取 
+                  if(token == operand) prefix_expr[count++] = expr[length+1]; //token是數字或未知數就輸出到prefix_expr中 
+                  else //token如果是運算子，stack為空(top=-1)或讀到token的優先度>=stack最上面token的優先度，push到stack 
                   {
-                      temp.data = token;
-                      if(token == leftparenthesis)
-                      {
+                      temp.data = token;  //token存到暫存的element中 
+                      if(token == leftparenthesis) //如果token為'('，代表')'已經在stack中 
+                      {                            //透過put_token輸出stack中的token直到碰到')' 
                                while(stack[top].data != rightparenthesis) put_token(prefix_expr,pop(),&count);
-                               pop();
+                               pop();              // 移除stack中的')' 
                       }
                       else if(top == -1 || icp[token] >= isp[stack[top].data]) push(temp);
-                      else
-                      {
-                          while(icp[token] < isp[stack[top].data]) put_token(prefix_expr,pop(),&count);
+                      else  //讀到token的優先度<stack最上面的token 
+                      {     //透過put_token輸出stack中的token直到token的優先度>=stack[top]才push 
+                          while(top != -1 && icp[token] < isp[stack[top].data]) put_token(prefix_expr,pop(),&count);
                           push(temp);
                       }
                   }
      }
-     while(top != -1) put_token(prefix_expr,pop(),&count);
-     for(i = 0 ; i < (count-1)/2 ; i++)
+     while(top != -1) put_token(prefix_expr,pop(),&count);  //字串讀完將剩下的運算子輸出 
+     for(i = 0 ; i < (count-(count%2))/2 ; i++) //因為結果是由後往前放(右往左讀)，所以最後反轉prefix_expr 
      {
            ctemp = prefix_expr[i];
            prefix_expr[i] = prefix_expr[count-1-i];
@@ -114,7 +196,7 @@ int main()
     int i;
     
     printf("please enter a infix expression:\n");
-    scanf("%s",expr);
+    gets(expr);
     
     infix_to_prefix(expr,prefix_expr,strlen(expr)-1);
     printf("%s\n",prefix_expr);
